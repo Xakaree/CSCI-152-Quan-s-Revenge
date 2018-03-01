ae5aae84a4da77d6d74856a7e6d6e8ef7cec46a9
ac488603 <ac488603@mail.fresnostate.edu>
2018-02-28 20:16:54 -0800

    arrow keys now mapped to buttons

--- scripts/controlMapping.js
+++ scripts/controlMapping.js
@@ -12,22 +12,53 @@ function controlMapping(){
       ctx1.fillStyle = "white";
       ctx1.fillRect(0,0,width,height);
 
-      ctx1.fillStyle = "grey";
+      if(this.cmOption == 1){ctx1.fillStyle = "green";}
+      else {ctx1.fillStyle = "grey";}
       ctx1.fillRect(290,290,150,50);//up
+
+      if(this.cmOption == 3){ctx1.fillStyle = "green";}
+      else {ctx1.fillStyle = "grey";}
       ctx1.fillRect(290,410,150,50);//down
+
+      if(this.cmOption == 4){ctx1.fillStyle = "green";}
+      else {ctx1.fillStyle = "grey";}
       ctx1.fillRect(205,350,150,50);//left
+
+      if(this.cmOption == 2){ctx1.fillStyle = "green";}
+      else {ctx1.fillStyle = "grey";}
       ctx1.fillRect(375,350,150,50);//right
 
+      if(this.cmOption == 5){ctx1.fillStyle = "green";}
+      else {ctx1.fillStyle = "grey";}
       ctx1.fillRect(560,290,250,75); //jump
-      ctx1.fillRect(560,385,250,75); //attack
 
+      if(this.cmOption == 6){ctx1.fillStyle = "green";}
+      else {ctx1.fillStyle = "grey";}
+      ctx1.fillRect(560,385,250,75); //attack
     }//end active
   }// end draw
 
   this.Update = function(){
     if(this.active){
-
-    }
+      if(input.keyPress(38) && this.cmOption == 1){
+        this.cmOption = 5;
+      }
+      else if (input.keyPress(39)){
+        this.cmOption = 2;
+      }
+      else if(input.keyPress(40)){
+        this.cmOption = 3;
+      }
+      else if(input.keyPress(37)){
+        this.cmOption = 4;
+      }
+      else if(input.keyPress(38)){
+        this.cmOption = 1;
+      }
+      else if(input.keyPress(32)){
+        this.cmOption = 6;
+      }
+    }//end active
   }//end upadte
 
 } // end controlMapping
