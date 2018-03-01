c4be5d6aa4d773aecf2a8ad248ab553e766af824
Zach <Zachary-2008@hotmail.com>
2018-02-28 18:06:48 -0800

    Included all the images in the main

--- scripts/animation.js
+++ scripts/animation.js
@@ -1,33 +1,6 @@
 //Scrapcode
 //New
-  var LCsprite={
-    v1Sprite:LC1,
-    v2Sprite:LC2,
-    frameLimits:[4,4,2,2,3,3,11,11],
-    width:64,
-    height:64
-  }
-  var QSsprite={
-    v1Sprite:QS1,
-    v2Sprite:QS2,
-    frameLimits:[4,4,2,2,3,3,11,11],
-    width:64,
-    height:64
-  }
-  var GZsprite={
-    v1Sprite:GZ1,
-    v2Sprite:GZ2,
-    frameLimits:[4,4,2,2,3,3,11,11],
-    width:64,
-    height:64
-  }
-  var SHsprite={
-    v1Sprite:SH1,
-    v2Sprite:SH2,
-    frameLimits:[4,4,2,2,3,3,11,11],
-    width:64,
-    height:64
-  }
+
 function animation(sprite,controls){
   //Animation Variables
   var srcX=0; //To track frame
---
+++ scripts/imagedata.js
@@ -0,0 +1,28 @@
+var LCsprite={
+  v1Sprite:LC1,
+  v2Sprite:LC2,
+  frameLimits:[4,4,2,2,3,3,11,11],
+  width:64,
+  height:64
+}
+var QSsprite={
+  v1Sprite:QS1,
+  v2Sprite:QS2,
+  frameLimits:[4,4,2,2,3,3,11,11],
+  width:64,
+  height:64
+}
+var GZsprite={
+  v1Sprite:GZ1,
+  v2Sprite:GZ2,
+  frameLimits:[4,4,2,2,3,3,11,11],
+  width:64,
+  height:64
+}
+var SHsprite={
+  v1Sprite:SH1,
+  v2Sprite:SH2,
+  frameLimits:[4,4,2,2,3,3,11,11],
+  width:64,
+  height:64
+}
