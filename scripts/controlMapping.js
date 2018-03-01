8a0d445e18597de664502df0db66883b8c8c07be
ac488603 <ac488603@mail.fresnostate.edu>
2018-02-28 19:50:17 -0800

    Control Mapping Buttons

--- scripts/controlMapping.js
+++ scripts/controlMapping.js
@@ -11,6 +11,16 @@ function controlMapping(){
       ctx1.clearRect(0,0,width,height);
       ctx1.fillStyle = "white";
       ctx1.fillRect(0,0,width,height);
+
+      ctx1.fillStyle = "grey";
+      ctx1.fillRect(290,290,150,50);//up
+      ctx1.fillRect(290,410,150,50);//down
+      ctx1.fillRect(205,350,150,50);//left
+      ctx1.fillRect(375,350,150,50);//right
+
+      ctx1.fillRect(560,290,250,75); //jump
+      ctx1.fillRect(560,385,250,75); //attack
+
     }//end active
   }// end draw
 
