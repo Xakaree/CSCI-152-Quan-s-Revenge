81737a57e886d1b71ce77ad0629b737ddda99bda
Joey Barajar <jbarajar@gmail.com>
2018-03-04 23:34:49 -0800

    Collision (#34)
    * added item pickup to player and ranged weapon
    
    * added item dropping + some physics tweaks
    
    * added camera + zoom
    
    * small tweaks to camera and test level
    
    * physics changes and experimenting with fixed tile size levels
    
    * added health, projectile damage, health bars, knockback, and death
    
    * added changing healthbar colors and camera smoothing
    
    * experimental collision changes
    
    * added entity to kill players if they fall off map
    
    * added quick restart
    
    * animation scale fix
    
    * 64 tile size
    
    * Changed rangeditem to TommyGun + Changed character size to 64x64
    
    * collision fixes and minor changes to player/entities
    
    * added the character portraits to the health bars
    
    * added the damage sprites to knockback

--- scripts/entity.js
+++ scripts/entity.js
@@ -1,6 +1,9 @@
 function Entity(x,y,w,h,tag) {
     this.active = true; //toggle phyics/collision (NEEDS WORKS)
 
+    //this.grav = 3000.0;
+    this.grav = 3000 * tileScale;
+
     //positions
     this.x = x;
     this.y = y;
@@ -10,6 +13,10 @@ function Entity(x,y,w,h,tag) {
     //acceleration
     this.ax = 0.0;
     this.ay = 0.0;
+
+    this.airfriction = 200.0 * tileScale;
+    this.friction = 700.0 * tileScale;
+
     //width and height of box collider
     this.width = w;
     this.height = h;
@@ -25,14 +32,46 @@ function Entity(x,y,w,h,tag) {
     this.getMidX = function() {return this.x + (this.width/2);}
     this.getMidY = function() {return this.y + (this.height/2);}
 
-    this.solidCollision = function(collider) {
-        //calculate a normalized distance between entity and colliding entity
-        var dx = (collider.entity.getMidX() - this.getMidX()) / (collider.entity.width/2);
-        var dy = (collider.entity.getMidY() - this.getMidY()) / (collider.entity.height/2);
-        //absolute values of the above variables
-        var absDx = Math.abs(dx);
-        var absDy = Math.abs(dy);
+    this.updatePhysics = function() {
+        if(this.active) {
+            /*
+                NOTE: when adjusting positions over time
+                values have to be multiplied by the interval for smoothing and to
+                keep them framerate independent (See below)
+            */
 
+            //apply velocity changes        
+            this.vx += this.ax * interval;
+            if(this.vx > 0 && this.vy != 0) {
+                this.vx = Math.max(0.0, this.vx - (this.airfriction * interval));
+            }
+            else if(this.vx < 0 && this.vy != 0) {
+                this.vx = Math.min(0.0, this.vx + (this.airfriction * interval));
+            }
+            this.vy += (this.ay * interval) + (this.grav * interval);
+            //apply positional changes based on velocity (this is capped at 20 pixels per frame right now)
+            if(this.vx > 0) this.x += Math.min(20*tileScale, this.vx * interval);
+            else this.x += Math.max(-20*tileScale, this.vx * interval);
+            if(this.vy > 0) this.y += Math.min(20*tileScale, this.vy * interval);
+            else this.y += Math.max(-20*tileScale, this.vy * interval);
+            
+        }
+    }
+
+    this.solidCollision = function(collider) {
+        var tx, ty;
+        if(this.vx > 0) tx = this.getMidX() - Math.min(10*tileScale, (this.vx * interval)/2);
+        else tx = this.getMidX() - Math.max(-10*tileScale, (this.vx * interval)/2);
+        if(this.vy > 0) ty = this.getMidY() - Math.min(10*tileScale, (this.vy * interval)/2);
+        else ty = this.getMidY() - Math.max(-10*tileScale, (this.vy * interval)/2);
+
+        //calculate a normalized distance between entity and colliding entity
+        var dx = (collider.entity.getMidX() - tx) / (collider.entity.width/2.0);
+        var dy = (collider.entity.getMidY() - ty) / (collider.entity.height/2.0);
+        //absolute values of the above variables
+        var absDx, absDy;
+        absDx = Math.abs(dx);
+        absDy = Math.abs(dy);
 
         /* collision return codes (numbers are this entity's position relative to collidee)
           4 0 4
@@ -43,39 +82,49 @@ function Entity(x,y,w,h,tag) {
         //if x and y absolutes are close to each other then entity is on a corner
         if(Math.abs(absDx - absDy) < 0.01) {
             if(dx < 0) { //approaching from right
-                this.x = collider.entity.getRight();
+                this.x = collider.entity.getRight()+1;
             }
             else { //appraoching from left
-                this.x = collider.entity.getLeft() - this.width;
+                this.x = collider.entity.getLeft() - this.width-1;
             }
             if(dy < 0) { //approaching from bottom
-                this.y = collider.entity.getBot();
+                this.y = collider.entity.getBot()+1;
             }
             else { //approaching from top
-                this.y = collider.entity.getTop() - this.height;
+                this.y = collider.entity.getTop() - this.height-1;
             }
             return 4;
         }
         else if(absDx > absDy) { //appraoching from the side
             if(dx < 0) { //approaching from the right
-                this.x = collider.entity.getRight();
+                this.x = collider.entity.getRight()+1;
+                this.vx = 0;
                 return 1;
             }
             else { //approaching from left
-                this.x = collider.entity.getLeft() - this.width;
+                this.x = collider.entity.getLeft() - this.width-1;
+                this.vx = 0;
                 return 3;
             }
         }
         else { //approaching from the top/bottom
             if(dy < 0) { //approaching from bottom
-                this.y = collider.entity.getBot();
+                this.y = collider.entity.getBot()+1;
+                //console.log("top)")
                 this.vy = 0;
                 return 2;
             }
             else { //approaching from top
-                this.y = collider.entity.getTop() - this.height;
+                this.y = collider.entity.getTop() - this.height-1;
                 this.vy = 0;
                 this.ay = 0;
+
+                if(this.vx > 0) {
+                    this.vx = Math.max(0.0, this.vx - (this.friction * interval));
+                }
+                else if(this.vx < 0) {
+                    this.vx = Math.min(0.0, this.vx + (this.friction * interval));
+                }
                 return 0;
             }
         }
@@ -83,15 +132,31 @@ function Entity(x,y,w,h,tag) {
 }
 
 function collisionCheck(collider, collidee) {
-    var l1 = collider.entity.getLeft();
-    var r1 = collider.entity.getRight();
-    var b1 = collider.entity.getBot();
-    var t1 = collider.entity.getTop();
+    if(collider instanceof Entity && collidee instanceof Entity) {
+        //console.log("Entity check");
+        var l1 = collider.getLeft();
+        var r1 = collider.getRight();
+        var b1 = collider.getBot();
+        var t1 = collider.getTop();
 
-    var l2 = collidee.entity.getLeft();
-    var r2 = collidee.entity.getRight();
-    var b2 = collidee.entity.getBot();
-    var t2 = collidee.entity.getTop();
+        var l2 = collidee.getLeft();
+        var r2 = collidee.getRight();
+        var b2 = collidee.getBot();
+        var t2 = collidee.getTop();
+    }
+    else {
+//console.log("Object check");
+        var l1 = collider.entity.getLeft();
+        var r1 = collider.entity.getRight();
+        var b1 = collider.entity.getBot();
+        var t1 = collider.entity.getTop();
+
+        var l2 = collidee.entity.getLeft();
+        var r2 = collidee.entity.getRight();
+        var b2 = collidee.entity.getBot();
+        var t2 = collidee.entity.getTop();
+    }
+    
 
 
     //checks if any edges of the box colliders are intersecting
--- scripts/player.js
+++ scripts/player.js
@@ -1,10 +1,9 @@
 function Player(x, y,w,h, controls, sprite) {
     this.animation = new animation(sprite,controls);
-    this.entity = new Entity(x,y,w,h,"player"); //attach entity to this object for physics/collision
-    this.speed = 350.0; //how fast player moves left/right
-    this.accel = 2.0; //UNUSED FOR NOW
-    this.decel = 2.0;
-    this.grav = 3000.0; //how fast player accelerates downward
+    this.entity = new Entity(x,y,64,64,"player"); //attach entity to this object for physics/collision
+    this.speed = 350.0 * tileScale; //how fast player moves left/right
+    this.accel = 2.0 * tileScale; //UNUSED FOR NOW
+    this.decel = 2.0 * tileScale;
     this.controls = controls || defaultcontrols;
 
 	this.sprite = sprite;
@@ -15,45 +14,90 @@ function Player(x, y,w,h, controls, sprite) {
     this.idling = false;
 
 
+    this.item = null;
+    this.dropCool = false; //items cant be picked up during dropcool
+    this.dropcnt = 0; //drop cooldown counter
+    this.dropTime = 5; //number of frames that drop cooldown lasts
+
+    this.knockback = false;
+    this.knockcnt = 0;
+    this.knockTime = 5;
+
+    this.health = 100;
+    this.isAlive = true;
+    this.drawPlayer = true;
+
+    //handles cooldown between dropping item and being able to pickup another
+    this.updateCnts = function() {
+        if(this.dropCool) this.dropcnt++;
+        if(this.dropcnt == this.dropTime) {
+            this.dropCool = false;
+            this.dropcnt = 0;
+        }
+
+        if(this.knockback) this.knockcnt++;
+        if(this.knockcnt == this.knockTime) {
+            this.knockback = false;
+            this.animation.curSprite = this.animation.bseSprite;
+            this.knockcnt = 0;
+        }
+    }
+
     //handles all responses to input
     this.movement = function() {
         if(input.keyDown(this.controls.left)) {
-            if(!this.jumping) {
-                this.entity.vx = -this.speed;
-            }
-            else {
-                this.entity.vx = Math.min(-this.speed, this.entity.vx - this.accel);
+            if(!this.knockback) {
+                if(!this.jumping) {
+                    this.entity.vx = -this.speed;
+                }
+                else {
+                    this.entity.vx = Math.min(-this.speed, this.entity.vx - this.accel);
+                }
             }
             this.facing = -1;
         } 
         if(input.keyDown(this.controls.right)) {
-            if(!this.jumping) {
-                this.entity.vx = this.speed;
-            }
-            else {
-                this.entity.vx = Math.max(this.speed, this.entity.vx + this.accel);
+            if(!this.knockback) {
+                if(!this.jumping) {
+                    this.entity.vx = this.speed;
+                }
+                else {
+                    this.entity.vx = Math.max(this.speed, this.entity.vx + this.accel);
+                }
             }
             this.facing = 1;
         } 
         if(!input.keyDown(this.controls.right) && !input.keyDown(this.controls.left)) {
             //if no input while on the ground player stops immediately
-            if(!this.jumping) {
+            if(!this.jumping && !this.knockback) {
                 this.entity.vx = 0.0;
             }
             //if in the air player slows down over time
-            else {
+            /*else {
                 if(this.entity.vx > 0) {
                     this.entity.vx = Math.max(0.0, this.entity.vx - this.decel);
                 }
                 else if(this.entity.vx < 0) {
                     this.entity.vx = Math.min(0.0, this.entity.vx + this.decel);
                 }
-            }
+            }*/
         } 
-        if(input.keyPress(this.controls.jump) && !this.jumping) {
-            this.entity.vy = -1100.0;
+        if(input.keyPress(this.controls.jump) && !this.jumping && !this.knockback) {
+            this.entity.vy = -1100.0 * tileScale;
             this.jumping = true;
         }
+
+        if(this.item != null && input.keyPress(this.controls.attack) && input.keyDown(this.controls.down)) {
+            this.item.drop(this.facing);
+            this.item = null;
+            this.dropCool = true;
+        }
+
+        else if(input.keyDown(this.controls.attack)) {
+            if(this.item != null) this.item.attack();
+        }
+
+        
     }
     this.aniChange = function() {
       if(!input.keyDown(this.controls.left) &&
@@ -100,22 +144,22 @@ function Player(x, y,w,h, controls, sprite) {
     */
     this.Update = function() {
 
-        //this.updateInput();
-        this.movement(); //respond to input
+        if(this.health <= 0) {
+            if(this.isAlive) {
+                if(this.item != null) this.item.drop(this.facing);
+                this.item = null;
+                this.isAlive = false;
+                this.entity.active = false;
+                this.drawPlayer = false;
+            }
+        }
+        
+        this.updateCnts(); //handles cooldown between dropping item and being able to pickup another
+        if(this.isAlive) this.movement(); //respond to input
         this.aniChange();
-        /*
-            NOTE: when adjusting positions over time
-            values have to be multiplied by the interval for smoothing and to
-            keep them framerate independent (See below)
-        */
+        this.entity.updatePhysics(); //apply acceleration, velocity to position
 
-        //apply velocity changes
-        this.entity.vx += this.entity.ax * interval;
-        this.entity.vy += (this.entity.ay * interval) + (this.grav * interval);
-        //apply positional changes based on velocity (this is capped at 20 pixels per frame right now)
-        this.entity.x += Math.min(20, this.entity.vx * interval);
-        this.entity.y += Math.min(20, this.entity.vy * interval);
-
+        
         this.animation.Update();
     }
 
@@ -125,21 +169,46 @@ function Player(x, y,w,h, controls, sprite) {
         if(collider.entity.tag == "player") {
 
         }
+        if(collider.entity.tag == "projectile" && collider.parent != this) {
+            this.knockback = true;
+            this.animation.curSprite = this.animation.dmgSprite;
+            this.knockcnt = 0;
+            this.entity.vx = collider.entity.vx
+            this.health -= 10;
+
+        }
+        if(collider.entity.tag == "dead") {
+            this.health = 0;
+            this.entity.active = false;
+        }
+        if(collider.entity.tag == "item") {
+            if(input.keyDown(this.controls.down) && input.keyPress(this.controls.attack)) {
+                if(this.item == null && !this.dropCool) {
+                    collider.pickUp(this);
+                    this.item = collider;
+                }
+            }
+        }
         if(collider.entity.tag == "solid") {
             switch(this.entity.solidCollision(collider)) {
                 case 0: //bottom collision
                     this.jumping = false;
                     break;
             }
+            if(this.item != null) this.item.updatePosition();
         }
 
     }
 
     //All draw calls must be done in this function
     this.Draw = function() {
-       // ctx1.fillStyle = color;
-       // ctx1.fillRect(this.entity.x,this.entity.y,this.entity.width,this.entity.height);
-	     //ctx1.drawImage(this.sprite,srcX*64,srcY*64,64,64,this.entity.x,this.entity.y,tileSize,tileSize)
-       this.animation.Draw(this.entity.x,this.entity.y,this.entity.width,this.entity.height);
+        if(this.drawPlayer) {
+            // ctx1.fillStyle = color;
+                //ctx1.fillRect(this.entity.x*scale,this.entity.y*scale,this.entity.width*scale,this.entity.height*scale);
+                //ctx1.drawImage(this.sprite,srcX*64,srcY*64,64,64,this.entity.x,this.entity.y,tileSize,tileSize)
+            this.animation.Draw(this.entity.x,this.entity.y,this.entity.width,this.entity.height);
+            if(this.item != null) this.item.manualDraw(this.entity.x, this.entity.y);
+        }
+       
     }
 }
