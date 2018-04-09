13f982285a82c2ceba978f86d4aa679f70b4361c
ac488603 <ac488603@mail.fresnostate.edu>
2018-04-08 23:21:22 -0700

    parallax
    -buttery smooth animation

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -18,8 +18,8 @@ function Parallax(camera, image1 = null, image2 = null, image3 = null){
         ctx1.save();
 
         ctx1.translate(-this.camera.x, -this.camera.y);
-        ctx1.drawImage(background, (xdir*3) + (-this.camera.x*0.5), (ydir*3) + (-this.camera.y*0.5),(canvas.width +1500) *scale,(canvas.height + 1500)*scale);
-        ctx1.drawImage(midground, (xdir*2) + (-this.camera.x*0.25), (ydir*2) + (-this.camera.y*0.25),(canvas.width + 750) *scale,(canvas.height +750)*scale);
+        ctx1.drawImage(background, (xdir*3) + (-this.camera.x*0.25), (ydir*3) + (-this.camera.y*0.25),(canvas.width +1500) *scale,(canvas.height + 1500)*scale);
+        ctx1.drawImage(midground, (xdir*2) + (-this.camera.x*0.20), (ydir*2) + (-this.camera.y*0.20),(canvas.width + 750) *scale,(canvas.height +750)*scale);
         ctx1.drawImage(foreground, xdir*scale, ydir*scale,(canvas.width + 450) *scale,(canvas.height + 550)*scale);
 
         ctx1.restore();
