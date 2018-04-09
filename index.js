e32f639505a4c70f6c5b7068e778d213c99be54f
Joey Barajar <jbarajar@gmail.com>
2018-04-09 02:41:03 -0700

    added node server + seperate desktop/phone pages

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var device = require('express-device');
+
+app.use(device.capture());
+
+//
+
+app.get('/', function(req, res){
+  console.log(req.device.type);
+  if(req.device.type == "desktop") {
+    app.use(express.static(__dirname + '/public'));
+    res.sendFile(__dirname + '/public/index.html');
+  }
+  else if(req.device.type == "phone") {
+    app.use(express.static(__dirname + '/public/phone'));
+    res.sendFile(__dirname + '/public/phone/index.html');
+  }
+});
+
+io.on('connection', function(socket){
+  console.log(socket.client.id + ' connected');
+});
+
+var port = 7778;
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
