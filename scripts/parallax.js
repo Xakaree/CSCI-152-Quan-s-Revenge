c48889dd9173c4f43d9cd06a7c7a150e5acea32e
ac488603 <ac488603@mail.fresnostate.edu>
2018-04-13 17:15:11 -0700

    Car Motion
    demo #1

--- scripts/parallax.js
+++ scripts/parallax.js
@@ -29,6 +29,10 @@ this.Update = function()
         ctx1.save();
 
         ctx1.translate(-this.camera.x, -this.camera.y);
+
+        ctx1.drawImage(background, (xdir*3) + (-this.camera.x*0.25), (ydir*3) + (-this.camera.y*0.25),(canvas.width +1500) *scale,(canvas.height + 1500)*scale);
+        ctx1.drawImage(midground, (xdir*2) + (-this.camera.x*0.20), (ydir*2) + (-this.camera.y*0.20),(canvas.width + 750) *scale,(canvas.height +750)*scale);
+        ctx1.drawImage(foreground, (xdir*scale) + 25, (ydir*scale) + 50,(canvas.width + 450) *scale,(canvas.height + 550)*scale);
         if(this.backgroundObjects != null)
         {
           for(let i =0; i < this.backgroundObjects.length; i++)
@@ -36,9 +40,6 @@ this.Update = function()
             this.backgroundObjects[i].Draw();
           }
         }
-        ctx1.drawImage(background, (xdir*3) + (-this.camera.x*0.25), (ydir*3) + (-this.camera.y*0.25),(canvas.width +1500) *scale,(canvas.height + 1500)*scale);
-        ctx1.drawImage(midground, (xdir*2) + (-this.camera.x*0.20), (ydir*2) + (-this.camera.y*0.20),(canvas.width + 750) *scale,(canvas.height +750)*scale);
-        ctx1.drawImage(foreground, (xdir*scale) + 25, (ydir*scale) + 50,(canvas.width + 450) *scale,(canvas.height + 550)*scale);
         ctx1.restore();
 
     }
--- scripts/scene.js
+++ scripts/scene.js
@@ -51,7 +51,7 @@ function Scene() {
     this.collisions = []; //list of collision to resolve
     this.items = [TommyGun, Shotgun, Flamethrower];
     this.camera = new Camera();
-    this.para =  new Parallax(this.camera, "background/Space.png","background/Planets.png","background/Moon.png",[new UFObgi(), new Spaceship()]); // beta features
+    this.para =  new Parallax(this.camera, "background/Skyscrapers.png","background/MoreBuildings.png","background/Buildings.png",[new Car()]); // beta features
     this.active = true;
 
     this.win = false;
