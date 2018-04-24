e21ab90b5045d1ca642a1596b18a3aae3c261d30
ac488603 <ac488603@mail.fresnostate.edu>
2018-04-24 16:23:43 -0700

    Gradient added
    - background lighting

--- public/scripts/stageData.js
+++ public/scripts/stageData.js
@@ -6,10 +6,10 @@ var stageData = {
     objs : [new UFObgi(), new Spaceship()]
   },
   1:{
-    background :  "background/Skyscrapers.png",
+    background : "background/Skyscrapers.png",
     midground  : "background/MoreBuildings.png",
     foreground  :"background/Buildings.png",
-    objs : [new Car()]
+    objs : [new Car(), new GradientForCity()]
   },
   2:{
     background :  "background/Rocks.png",
@@ -22,3 +22,20 @@ var stageData = {
     foreground  :"background/Ship.png",
   }
 };
+
+function GradientForCity(){
+  this.Update = function()
+  {
+
+  }
+
+  this.Draw =  function()
+  {
+    var my_gradient=ctx1.createLinearGradient(0,0,0,720);
+    my_gradient.addColorStop(0,"#191970");
+    my_gradient.addColorStop(0.35,"#4B0082");
+    my_gradient.addColorStop(1,"#ffffe0");
+    ctx1.fillStyle=my_gradient;
+    ctx1.fillRect(-250,-250,width+250,height+250);
+  }
+}
