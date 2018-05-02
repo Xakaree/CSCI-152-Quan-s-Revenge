19682e6876584ff2f74e8514bc635ebe105596bb
Joey Barajar <jbarajar@gmail.com>
2018-05-02 00:15:19 -0700

    fix for multiple mobile controllers

--- public/scripts/input.js
+++ public/scripts/input.js
@@ -242,14 +242,6 @@ function InputHandler() {
 
 InputHandler.prototype.initSocket = function() {
     var socket = io();
-    this.oldinp = {
-        0: false,
-        1: false,
-        2: false,
-        3: false,
-        5: false,
-        4: false
-    }
 
     socket.emit('coderequest');
 
@@ -276,21 +268,18 @@ InputHandler.prototype.initSocket = function() {
 }
 
 InputHandler.prototype.onInput = function(id, inp) {
-    //if(!oldinp) oldinp = Object.assign({}, inp);
-    //console.log(inp);
     for(let key in inp) {
         if(inp[key]) {
             if(!econtrols[id]) {
                 console.log("making new controller");
                 econtrols[id] = new mobile(id);
             }
-            if(inp[key] != this.oldinp[key]) input.serverDown(id,key);
+            input.serverDown(id,key);
         }
         else {
-            if(inp[key] != this.oldinp[key]) input.serverUp(id, key)
+            input.serverUp(id, key)
         }
     }
-    this.oldinp = Object.assign({}, inp);
 }
 
 
